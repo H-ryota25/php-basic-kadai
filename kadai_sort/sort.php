e71904db117965d61d918b117ec8377cf57b73c9
H-ryota25 <hashiryota25@gmail.com>
2025-02-25 20:34:40 +0900

    Add files via upload
    課題ソート　再提出

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -9,28 +9,30 @@
 <body>
     <p>
         <?php
-       function sort_2way(&$array, $order) {
-           if ($order) {
-               sort($array);
-           } else {
-               rsort($array);
-           }
-       }
-       
-       $nums = [15, 4, 18, 23, 10];
-       
-       echo "昇順にソートします。<br>";
-       sort_2way($nums, true);
-       foreach ($nums as $num) {
-           echo $num . "<br>";
-       }
-       
-       echo "<br>降順にソートします。<br>";
-       sort_2way($nums, false);
-       foreach ($nums as $num) {
-           echo $num . "<br>";
-       }
+        function sort_2way($array, $order) {
+            if ($order) {
+                sort($array);
+            } else {
+                rsort($array);
+            }
+            return $array; // ソートされた配列を返す
+        }
+        
+        $nums = [15, 4, 18, 23, 10];
+        
+        echo "昇順にソートします。<br>";
+        $sorted_nums = sort_2way($nums, true);
+        foreach ($sorted_nums as $num) {
+            echo $num . "<br>";
+        }
+        
+        echo "<br>降順にソートします。<br>";
+        $sorted_nums = sort_2way($nums, false);
+        foreach ($sorted_nums as $num) {
+            echo $num . "<br>";
+        }
         ?>
+        
     </p>
 </body>
 
